264e46ae0d11c13c2d9d994c24124303f5a6bc9f
Buldadavid <104922435+Buldadavid@users.noreply.github.com>
2022-05-05 17:38:45 +0200

    Update epg.py

--- TV/epg.py
+++ TV/epg.py
@@ -1,9 +1,7 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from datetime import date
-import sys
-
-sys.stdout = open("/home/pi/TV/epg.txt", "w")
+import os
 
 today = date.today()
 d1 = today.strftime("%d")
@@ -19,6 +17,15 @@ tree = ET.parse('/home/pi/TV/Programy/iptvGuide.xml')
 root = tree.getroot()
 #print(root)
 
+with open('epg.html', 'a') as ff:
+    ff.write('<!DOCTYPE html>\n')
+    ff.write('<html><head><link rel="stylesheet" href="static/mystyle.css">\n')
+    ff.write('<meta charset="8859">\n')
+    ff.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
+    ff.write('<title>Ovladač</title>\n')
+    ff.write('</head><style></style><body><table>\n')
+    ff.close() 
+
 for programme in root.findall('programme'):
     porad = programme.find('title').text
     start = programme.attrib['start']
@@ -26,41 +33,60 @@ for programme in root.findall('programme'):
     channel = programme.attrib['channel']
     za = start[8:10]+":"+start[10:12]
     ko = stop[8:10]+":"+stop[10:12]
+    
+    def fce():
+        if ko > cctime and za < cctime:
+            barva = "nic"
+        else:
+            barva = "nic2"
+
+        with open('epg.html', 'a') as ff:
+            ff.write(f"<tr><td id= '{barva}'>{kanal} {za} {ko} {porad}</td></tr>\n")
+            ff.close()            
 
     if channel == "36e6c878e5fef604094a70bacee2cd6c" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "ČT1"
-        print(kanal,za,ko,porad)
+        fce()
         
     if channel == "68043b642284e246c82723178b18f846" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "ČT2"
-        print(kanal,za,ko,porad)
+        fce()
 
     if channel == "f3ea5080f442773e4843666b76e22ed1" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "NOVA"
-        print(kanal,za,ko,porad)
+        fce()
         
     if channel == "92ee8c12c8787b3fd4545204591abe18" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "NOVA CINEMA"
-        print(kanal,za,ko,porad)
+        fce()
 
     if channel == "a8ece1d3e98e4807ac05b0eef675f062" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "NOVA FUN"
-        print(kanal,za,ko,porad)
+        fce()
         
     if channel == "3eac239d56747bf05c95fc73c63deede" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "PRIMA"
-        print(kanal,za,ko,porad)
+        fce()
 
     if channel == "e115399635e9fef8f9c120f023e3d3db" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "PRIMA COOL"
-        print(kanal,za,ko,porad)
+        fce()
         
     if channel == "80149d93f0764c1b82aa86494deb0245" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "PRIMA ZOOM"
-        print(kanal,za,ko,porad)
+        fce()
         
     if channel == "5aaf422ee23108e38907319f77a96421" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
         kanal = "PRIMA MAX"
-        print(kanal,za,ko,porad)
-        
-sys.stdout.close()
+        fce()
+
+with open('epg.html', 'a') as ff:
+    ff.write('</table>\n')
+    ff.write('<a href="http://10.0.0.200:5000">\n')
+    ff.write('    <input type="button" value="Zpět" id = "greet1" />\n')
+    ff.write('</a>\n')
+    ff.write('</body>\n')
+    ff.write('</html>\n')
+    ff.close() 
+
+os.system("mv epg.html /home/pi/TV/templates/epg.html")
