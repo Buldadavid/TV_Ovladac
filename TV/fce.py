dbeecf31bf821fb7e7ca66f6c8243b357805772f
Buldadavid <104922435+Buldadavid@users.noreply.github.com>
2022-05-06 20:21:25 +0200

    Update fce.py

--- TV/fce.py
+++ TV/fce.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, redirect
 import os
 import pyautogui
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from datetime import date
+import os.path
+import time
 
 app = Flask(__name__)
 
@@ -48,8 +53,120 @@ def down():
 @app.route('/program', methods = ['POST', 'GET'])
 def program():
     print("program")
-    os.system("wget http://localhost:9981/xmltv/channels --user=admin --password=admin -O /home/pi/TV/Programy/iptvGuide.xml")
-    os.system("python3 /home/pi/TV/epg.py")
+    
+    os.system("rm templates/epg.html ")
+    
+    today = date.today()
+    d1 = today.strftime("%d")
+    m1 = today.strftime("%m%d")
+    #print(d1)
+    #print(m1)
+
+    now = datetime.now()
+    ctime = int(now.strftime("%H"))
+    cctime = now.strftime("%H:%M")
+    ctimee = (int(ctime) +3)
+    #print(ctimee)
+
+    tree = ET.parse('/home/pi/TV/Programy/iptvGuide.xml')
+    root = tree.getroot()
+    #print(root)
+    prog = [programme for programme in tree.findall('programme')]
+
+    last = prog[-1]
+    stopL = last.attrib['stop']
+    #print(stopL[4:8])
+
+    if m1 < stopL[4:8] :
+        print("jeste jo")
+        
+    else:
+        print("uz ne")
+        os.system("wget http://localhost:9981/xmltv/channels --user=admin --password=admin -O /home/pi/TV/Programy/iptvGuide.xml")
+
+    with open('templates/epg.html', 'a') as ff:
+        ff.write('{% extends "layout.html" %}}\n')
+        ff.write('{% block body %}\n')
+        ff.write('<h1>TV EPG</h1>\n')
+        ff.write('<table>\n')
+        ff.close() 
+
+    tree = ET.parse('/home/pi/TV/Programy/iptvGuide.xml')
+    root = tree.getroot()
+
+    for programme in root.findall('programme'):
+        porad = programme.find('title').text
+        start = programme.attrib['start']
+        stop = programme.attrib['stop']
+        channel = programme.attrib['channel']
+        za = start[8:10]+":"+start[10:12]
+        ko = stop[8:10]+":"+stop[10:12]
+
+        def fce():
+            if ko > cctime and za <= cctime:
+                barva = "nic"
+            else:
+                barva = "nic2"
+            with open('templates/epg.html', 'a') as ff:
+                if barva == "nic":
+                    ff.write(f"<tr><td id= '{barva}'>{kanal} {za} {ko} {porad}<a href='http://10.0.0.200:5000/{route}'><input type='submit' value='>' id= '{barva}'></a></td></tr>\n")
+                    ff.close()
+                if barva == "nic2":
+                    ff.write(f"<tr><td id= '{barva}'>{kanal} {za} {ko} {porad}</td></tr>\n")
+                    ff.close()             
+            
+        if channel == "36e6c878e5fef604094a70bacee2cd6c" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "ČT1"
+            route = "CT_1_HD_T2"
+            fce()
+            
+        if channel == "68043b642284e246c82723178b18f846" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "ČT2"
+            route = "CT_2_HD_T2"
+            fce()
+
+        if channel == "f3ea5080f442773e4843666b76e22ed1" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "NOVA"
+            route = "Nova"
+            fce()
+            
+        if channel == "92ee8c12c8787b3fd4545204591abe18" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "NOVA CINEMA"
+            route = "Nova_Cinema"
+            fce()
+
+        if channel == "a8ece1d3e98e4807ac05b0eef675f062" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "NOVA FUN"
+            route = "Nova_Fun"
+            fce()
+            
+        if channel == "3eac239d56747bf05c95fc73c63deede" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "PRIMA"
+            route = "Prima"
+            fce()
+
+        if channel == "e115399635e9fef8f9c120f023e3d3db" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "PRIMA COOL"
+            route = "Prima_COOL"
+            fce()
+            
+        if channel == "80149d93f0764c1b82aa86494deb0245" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "PRIMA ZOOM"
+            route = "Prima_ZOOM"
+            fce()
+            
+        if channel == "5aaf422ee23108e38907319f77a96421" and start[6:8] == d1 and int(start[8:10])<ctimee and ko>=cctime:
+            kanal = "PRIMA MAX"
+            route = "Prima_MAX"
+            fce()
+
+    with open('templates/epg.html', 'a') as ff:
+        ff.write('</table>\n')
+        ff.write('<a href="http://10.0.0.200:5000">\n')
+        ff.write('    <input type="button" value="Zpět" id = "greet1" />\n')
+        ff.write('</a>\n')
+        ff.write('{% endblock %}\n')
+        ff.close() 
     return render_template('epg.html')
 
 @app.route('/NOVA', methods = ['POST', 'GET'])
@@ -368,4 +485,13 @@ def UKRAJINSKE_RADIO():
      os.system('xdg-open /home/pi/TV/Programy/UKRAJINSKE_RADIO')
      return redirect('/')
 
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
 app.run(host='10.0.0.200', port=5000)
+
